9916178572fdf9c89bdb4b48f21e6fc2c27954ec
jose.dantas@jazzhr.com <jose.dantas@jazzhr.com>
2025-08-26 11:29:09 -0300

    feat: add knex and other libraries

---
+++ knexfile.ts
@@ -0,0 +1,56 @@
+import type { Knex } from "knex";
+
+// Update with your config settings.
+
+const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: "postgresql",
+    connection: {
+      database: "top_finance",
+      user: "postgres",
+      password: "D0us5xm4"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "top_finance",
+      user: "postgres",
+      password: "D0us5xm4"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "top_finance",
+      user: "postgres",
+      password: "D0us5xm4"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  }
+
+};
+
+module.exports = config;
