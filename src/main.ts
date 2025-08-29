33773fa8afaf3f1a5083d980b2ca7340c91a1e41
jose.dantas@jazzhr.com <jose.dantas@jazzhr.com>
2025-08-29 13:43:07 -0300

    feat: cors config

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Enable CORS for specific origins, methods, and headers
+  app.enableCors({
+    origin: [
+      'http://127.0.0.1:5002',
+      'http://localhost:5002',
+    ],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    credentials: true,
+  });
+
+  await app.listen(3001);
 }
 bootstrap();
